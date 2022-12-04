a9b737c16e6c7d2d751e94327620489dbca7021e
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2022-12-05 01:12:42 +0800

    3rd commit - method definition for dispenser class is complete

--- candy_machine.py
+++ candy_machine.py
@@ -4,19 +4,21 @@ class Candy_Machine:
     
     def program():
         """  """
+    # TODO
+
     def sell_product():
         """  """
+    # TODO
+
     def show_product():
         """  """
+    # TODO
 
 
 class Cash_Register:
 
     def __init__(self, cash_on_hand=500):
         self.cash_on_hand = cash_on_hand
-
-    def __str__(self) -> str:
-        return f"Cash on hand is ${self.cash_on_hand:,.2f}"
 
     # Getter
     @property
@@ -34,15 +36,19 @@ class Cash_Register:
         else:
             raise TypeError("Cash on Hand must be an integer")
 
+    def __str__(self):
+        return f"Cash on hand is ${self.cash_on_hand:,.2f}"
+
     def cash_register(self, cash_in):
         """ """
+        # TODO
 
     def current_balance(self):
-        """shows the current amount in the cash register"""
+        """ shows the current amount in the cash register """
         return self.cash_on_hand
 
     def accept_amount(self, amount_in):
-        """accepts the amount entered by the customer"""
+        """ accepts the amount entered by the customer """
         if isinstance(amount_in, int) and amount_in > 0:
             self.cash_on_hand += amount_in
         
@@ -51,23 +57,70 @@ class Cash_Register:
 
 
 class Dispenser:
-    def __init__(self, cost, number_of_items):
-        """  """
+    def __init__(self, cost=50, number_of_items=50):
+        self.cost = cost
+        self.number_of_items = number_of_items
+
+    # Getter
+    @property
+    def cost(self):
+        return self._cost
+
+    # Setter
+    @cost.setter
+    def cost(self, cost):
+        if isinstance(cost, int):
+            if cost < 0:
+                self._cost = 50 
+            
+            else:
+                self._cost = cost 
+        else:
+            raise TypeError("Cost must be an integer")
+
+    # Getter
+    @property
+    def number_of_items(self):
+        return self._number_of_items
+
+    # Setter
+    @number_of_items.setter
+    def number_of_items(self, number_of_items):
+        if isinstance(number_of_items, int):
+            if number_of_items < 0:
+                self._number_of_items = 50 
+            
+            else:
+                self._number_of_items = number_of_items 
+        else:
+            raise TypeError("Number of Items must be an integer")
+
+    def __str__(self):
+        return f"Cost is ${self.cost:,.2f} and Number of Items is {self.number_of_items}"
 
     def dispenser(self, set_no_of_items, set_cost):
         """  """
 
     def get_count(self):
-        """  """
+        """ returns the number of items of a particular product """
+        return self.number_of_items
 
     def get_product_cost(self):
-        """  """
+        """ returns the cost of a product """
+        return self.cost
 
     def makeSale(self):
-        """  """
+        """ Product sold, reduce number of items by 1 """
+        self.number_of_items -= 1
 
 
-a = Cash_Register(1000)
-a.accept_amount(3)
-a.accept_amount(5)
-print(a.current_balance())
+
+""" TESTINGS """
+# a = Cash_Register(1000)
+# a.accept_amount(3)
+# a.accept_amount(5)
+# print(a)
+
+b = Dispenser(3, 5)
+b.makeSale()
+print(b.get_count())
