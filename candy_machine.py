2837403ee927198d44ef19366b27fbdacfd37021
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2022-12-08 04:13:35 +0800

    13th commit - edit some comment

--- candy_machine.py
+++ candy_machine.py
@@ -1,10 +1,7 @@
-# TODO
-# put the admin changes to a func 
-
-
 import sys
 
 def main():
+    # Create the candy machine
     candy_machine = Candy_Machine()
 
     """ Optional - uncomment then edit default values in registers and dispensers """
@@ -14,6 +11,7 @@ def main():
     # candy_machine.gum_dispenser.dispenser(set_cost=25, set_no_of_items=20)
     # candy_machine.cookie_dispenser.dispenser(set_cost=25, set_no_of_items=20)
 
+    # Start the program
     candy_machine.program()
 
 
@@ -21,11 +19,11 @@ class Candy_Machine:
 
     def __init__(self):
         """ Initalize the components of candy machine """
-        self.cash_register = Cash_Register()
-        self.candy_dispenser = Dispenser()
-        self.chip_dispenser = Dispenser()
-        self.gum_dispenser = Dispenser()
-        self.cookie_dispenser = Dispenser()
+        self.cash_register = self.Cash_Register()
+        self.candy_dispenser = self.Dispenser()
+        self.chip_dispenser = self.Dispenser()
+        self.gum_dispenser = self.Dispenser()
+        self.cookie_dispenser = self.Dispenser()
         
         # Key mapping in selection menu
         self.item_key = {"1": {"item": "candy", "dispenser": self.candy_dispenser},
@@ -44,27 +42,29 @@ class Candy_Machine:
                           "S": "set_balance",
                           "Q": "back"}
 
-    # Getter
+    # Choice Getter
     @property
     def choice(self):
         return self._choice
 
-    # Setter
+    # Choice Setter
     @choice.setter
     def choice(self, choice):
+        # Ensure the choice maps to item key
         if not choice or choice not in self.item_key:
             raise ValueError("Choice is not in item key")
         else:
             self._choice = self.item_key[choice]
 
-    # Getter
+    # Admin Choice Getter
     @property
     def admin_choice(self):
         return self._admin_choice
 
-    # Setter
+    # Admin Choice Setter
     @admin_choice.setter
     def admin_choice(self, admin_choice):
+        # Ensure the choice maps to item key
         if not admin_choice or admin_choice not in self.admin_key:
             raise ValueError("Choice is not in admin key")
         else:
@@ -251,105 +251,108 @@ class Candy_Machine:
         self.admin_choice = input("\nYour choice:  ")[0].upper() 
 
 
-class Cash_Register():
-    """ component of Candy Machine, handles money """
+    # Component (inner class) of Candy Machine
+    class Cash_Register():
+        """ Handles money """
 
-    def __init__(self, cash_on_hand=500):
-        self.cash_on_hand = cash_on_hand
+        def __init__(self, cash_on_hand=500):
+            self.cash_on_hand = cash_on_hand
 
-    # Getter
-    @property
-    def cash_on_hand(self):
-        return self._cash_on_hand
+        # Getter
+        @property
+        def cash_on_hand(self):
+            return self._cash_on_hand
 
-    # Setter
-    @cash_on_hand.setter
-    def cash_on_hand(self, cash_on_hand):
-        if isinstance(cash_on_hand, int):
-            if cash_on_hand < 0:
-                self._cash_on_hand = 500
+        # Setter
+        @cash_on_hand.setter
+        def cash_on_hand(self, cash_on_hand):
+            if isinstance(cash_on_hand, int):
+                if cash_on_hand < 0:
+                    self._cash_on_hand = 500
+                else:
+                    self._cash_on_hand = cash_on_hand
             else:
-                self._cash_on_hand = cash_on_hand
-        else:
-            raise TypeError("Cash on Hand must be an integer")
+                raise TypeError("Cash on Hand must be an integer")
 
-    def __str__(self):
-        return f"Cash on hand is ${self.cash_on_hand:,.2f}"
+        def __str__(self):
+            return f"Cash on hand is ${self.cash_on_hand:,.2f}"
 
-    def cash_register(self, cash_in=500):
-        """ Let candy machine modify cash on hand """
-        self.cash_on_hand = cash_in
+        def cash_register(self, cash_in=500):
+            """ Let candy machine modify cash on hand """
+            self.cash_on_hand = cash_in
 
-    def current_balance(self):
-        """ Shows the current amount in the cash register """
-        return self.cash_on_hand
+        def current_balance(self):
+            """ Shows the current amount in the cash register """
+            return self.cash_on_hand
 
-    def accept_amount(self, amount_in):
-        """ Accepts the amount entered by the customer """
-        if isinstance(amount_in, int) and amount_in > 0:
-            self.cash_on_hand += amount_in
+        def accept_amount(self, amount_in):
+            """ Accepts the amount entered by the customer """
+            if isinstance(amount_in, int) and amount_in > 0:
+                self.cash_on_hand += amount_in
+            
+            else:
+                raise TypeError("Amount In must be non negative integer")
+
+
+    # Component (inner class) of Candy Machine
+    class Dispenser:
+        """ Handles product """
         
-        else:
-            raise TypeError("Amount In must be non negative integer")
+        def __init__(self, cost=50, number_of_items=50):
+            self.cost = cost
+            self.number_of_items = number_of_items
 
+        # Cost Getter
+        @property
+        def cost(self):
+            return self._cost
 
-class Dispenser:
-    """ Component of Candy Machine, handles product """
-    def __init__(self, cost=50, number_of_items=50):
-        self.cost = cost
-        self.number_of_items = number_of_items
-
-    # Getter
-    @property
-    def cost(self):
-        return self._cost
-
-    # Setter
-    @cost.setter
-    def cost(self, cost):
-        if isinstance(cost, int):
-            if cost <= 0:
-                self._cost = 50
+        # Cost Setter
+        @cost.setter
+        def cost(self, cost):
+            if isinstance(cost, int):
+                if cost <= 0:
+                    self._cost = 50
+                else:
+                    self._cost = cost 
             else:
-                self._cost = cost 
-        else:
-            raise TypeError("Cost must be an integer")
+                raise TypeError("Cost must be an integer")
 
-    # Getter
-    @property
-    def number_of_items(self):
-        return self._number_of_items
+        # Stock Getter
+        @property
+        def number_of_items(self):
+            return self._number_of_items
 
-    # Setter
-    @number_of_items.setter
-    def number_of_items(self, number_of_items):
-        if isinstance(number_of_items, int):
-            if number_of_items < 0:
-                self._number_of_items = 50 
+        # Stock Setter
+        @number_of_items.setter
+        def number_of_items(self, number_of_items):
+            if isinstance(number_of_items, int):
+                if number_of_items < 0:
+                    self._number_of_items = 50 
+                else:
+                    self._number_of_items = number_of_items 
             else:
-                self._number_of_items = number_of_items 
-        else:
-            raise TypeError("Number of Items must be an integer")
+                raise TypeError("Number of Items must be an integer")
 
-    def __str__(self):
-        return f"Cost is ${self.cost:,.2f} and Number of Items is {self.number_of_items}"
+        def __str__(self):
+            return f"Cost is ${self.cost:,.2f} and Number of Items is {self.number_of_items}"
 
-    def dispenser(self, set_cost=50, set_no_of_items=50):
-        """ Let candy machine modify number of items and cost of item """
-        self.cost = set_cost
-        self.number_of_items = set_no_of_items
+        def dispenser(self, set_cost=50, set_no_of_items=50):
+            """ Let candy machine modify number of items and cost of item """
+            self.cost = set_cost
+            self.number_of_items = set_no_of_items
 
-    def get_count(self):
-        """ returns the number of items of a particular product """
-        return self.number_of_items
+        def get_count(self):
+            """ returns the number of items of a particular product """
+            return self.number_of_items
 
-    def get_product_cost(self):
-        """ returns the cost of a product """
-        return self.cost
+        def get_product_cost(self):
+            """ returns the cost of a product """
+            return self.cost
 
-    def makeSale(self):
-        """ Product sold, reduce number of items by 1 """
-        self.number_of_items -= 1
+        def makeSale(self):
+            """ Product sold, reduce number of items by 1 """
+            self.number_of_items -= 1
 
 
 if __name__ == "__main__":
